8907baa0671aaa95c0c6b8d1bc36f64c7476d1b7
Like Xu <likexu@tencent.com>
2023-03-09 15:40:11 +0800

    Init: Download All AMD SEC spec files, here we go!
    Ref: https://developer.amd.com/sev/

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-check-certificate https://developer.amd.com/sev/ -O amd-sev.html
+wget --no-check-certificate https://specs.openstack.org/openstack/nova-specs/specs/stein/approved/amd-sev-libvirt-support.html -O amd-sev-libvirt.html
+
